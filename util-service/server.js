8f172866b0c50165a937387ead9a955766aa4f41
Matt Miller <thisismattmiller@gmail.com>
2021-03-23 08:13:02 -0400

    update

--- util-service/server.js
+++ util-service/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const shell = require('shelljs')
 const cors = require('cors')
+const rp = require('request-promise');
 
 
 
@@ -11,8 +12,14 @@ const MongoClient = mongo.MongoClient;
 var recsStageByEid = {}
 var recsProdByEid = {}
 
+
 var recsStageByUser = {}
 var recsProdByUser = {}
+
+const MLUSER = process.env.MLUSER;
+const MLPASS = process.env.MLPASS;
+const STAGINGPOSTURL = process.env.STAGINGPOSTURL;
+
 
 
 
@@ -23,7 +30,6 @@ MongoClient.connect(uri, function(err, client) {
 
 
     // build an intial index
-    console.log('yeeeeee')
     // db.collection('resourcesStaging').find({}, {}, 0, 1, function (err, docs) {
     //     if(err){
     //         throw err;
@@ -39,7 +45,6 @@ MongoClient.connect(uri, function(err, client) {
  		cursor.forEach((doc)=>{
 
  			if (doc.index){
- 				console.log(doc.index)
  				if (doc.index.eid){
  					recsStageByEid[doc.index.eid] = doc.index
  				}
@@ -61,8 +66,6 @@ MongoClient.connect(uri, function(err, client) {
 				.then(function(doc) {
         if(!doc)
             throw new Error('No record found.');
-          	console.log('--------------')
-			      console.log(doc.index);//else case
 
 			      // add it to the list or update it whatever
 		 				if (doc.index.eid){
@@ -90,15 +93,121 @@ MongoClient.connect(uri, function(err, client) {
 
 var app = express();
 
-app.use(express.json());
+app.use(express.json({limit: '15mb'}));
 
-app.use(cors())
+app.use(cors({origin:true}))
+
 app.options('*', cors())
 
 
 app.get('/', function(request, response){
   console.log(request.body);      // your JSON
    response.send(request.body);    // echo the result back
+});
+
+
+app.post('/error/report', (request, response) => {
+
+    MongoClient.connect(uri, function(err, db) {
+        if (err) throw err;
+        var dbo = db.db("bfe2");
+
+        // turn it back into a string for storage because mongo is fusssy about key IDs
+        request.body.activeProfile = JSON.stringify(request.body.activeProfile)
+        dbo.collection("errorReports").insertOne(request.body, 
+        function(err, result) {
+            if (err) {
+            	response.json({'result':false,'error':err});
+            }
+            response.json({'result':true,'error':err});
+            db.close();
+        });
+    });
+});
+
+app.get('/error/report', (request, response) => {
+
+    MongoClient.connect(uri, function(err, db) {
+        if (err) throw err;
+        var dbo = db.db("bfe2");
+		    var cursor = dbo.collection('errorReports').find({});
+		    let results = []
+		 		cursor.forEach((doc)=>{
+		 			results.push({eId:doc.eId,desc:doc.desc,contact:doc.contact})
+		 		}, function(err) {
+			 		console.log(results)
+			 		response.json(results)
+				})
+
+
+    });
+});
+
+app.post('/publish/staging', (request, response) => {
+
+	// var shortuuid = require('short-uuid');
+	// var decimaltranslator = shortuuid("0123456789");
+	// var objid = req.body.objid;
+	// var lccn = req.body.lccn;
+	// var dirname = __dirname + resources;
+
+	var name = request.body.name + ".rdf";
+	var rdfxml = request.body.rdfxml; 
+
+	var url = "https://" + STAGINGPOSTURL + "/post/" + name;
+	var options = {
+	    method: 'POST',
+	    uri: url,
+	    body: rdfxml,
+	    headers: { "Content-type": "application/xml" },
+	    auth: {
+	            'user': MLUSER,
+	            'pass': MLPASS,
+	        },
+	    json: false // Takes JSON as string and converts to Object
+	};
+	rp(options)
+	    .then(function (data) {
+	        // {"name": "72a0a1b6-2eb8-4ee6-8bdf-cd89760d9f9a.rdf","objid": "/resources/instances/c0209952430001",
+	        // "publish": {"status": "success","message": "posted"}}
+	        console.log(data);
+	        data = JSON.parse(data);
+	        console.log(data.objid)
+	        
+	        var resp_data = {}
+	        if (data.publish.status == "success") {
+	            // IF successful, it is by definition in this case also posted.
+	            resp_data = {
+	                    "name": request.body.name, 
+	                    "url": resources + name, 
+	                    "objid": data.objid, 
+	                    "lccn": lccn, 
+	                    "publish": {"status":"published"}
+	                }
+	        } else {
+	            resp_data = {
+	                    "name": rerequestq.body.name, 
+	                    "objid":  data.objid, 
+	                    "publish": {"status": "error","message": data.publish.message }
+	                }
+	        }
+	        res.set('Content-Type', 'application/json');
+	        res.status(200).send(resp_data);
+	    })
+	    .catch(function (err) {
+	        // POST failed...
+	        console.log(err)
+	        resp_data = {
+	                "name": request.body.name, 
+	                "objid":  "objid", 
+	                "publish": {"status": "error","message": err }
+	            }
+	        response.set('Content-Type', 'application/json');
+	        response.status(500).send(resp_data);
+	    });
+
+
+   
 });
 
 app.get('/myrecords/staging/:user', function(request, response){
