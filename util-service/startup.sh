cece123b16d0f994e2d6babcfc9918c35305c2b6
Matt Miller <thisismattmiller@gmail.com>
2022-04-14 17:20:03 -0400

    ignore tmp folder on util server

--- util-service/startup.sh
+++ util-service/startup.sh
@@ -16,7 +16,7 @@ echo $MODE
 if [ $MODE = 1 ]
 then
 	echo "Doing dev mode"
-    nodemon server.js --ignore '*.json'
+    nodemon server.js --ignore '*.json' --ignore /app/tmp/ --ignore app/tmp/ --ignore tmp/
 else
 	echo "Doing prod mode"
     forever server.js
