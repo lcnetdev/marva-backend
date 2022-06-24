459d9ddc38043657878a98cddb74de8fb08de610
Matt Miller <thisismattmiller@gmail.com>
2022-06-24 17:23:16 -0400

    more config stuff

--- config/ldpjs_startup.sh
+++ config/ldpjs_startup.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-npm install -g nodemon
+# npm install -g nodemon
 npm install
-#forever server.js
-nodemon server.js --ignore '*.json' --ignore '*.xml' --ignore /app/tmp/ --ignore app/tmp/ --ignore tmp/
+npm install --save cors fast-xml-parser strnum
+forever server.js
+# nodemon server.js --ignore '*.json' --ignore '*.xml' --ignore /app/tmp/ --ignore app/tmp/ --ignore tmp/
