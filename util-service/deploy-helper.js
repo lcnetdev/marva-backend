98be44b7886720b3d3743962aebda3ef70348325
Matt Miller <thisismattmiller@gmail.com>
2022-07-20 17:27:51 -0400

    config profile-editor deployment path

--- util-service/deploy-helper.js
+++ util-service/deploy-helper.js
@@ -1,5 +1,7 @@
 const process = require( 'process' );
 const fs = require('fs').promises;
+const fsOrg = require('fs');
+
 
 const argv = key => {
   // Return true if the key exists and a value is defined
@@ -67,3 +69,49 @@ if (argv('action') == 'deploy_marva_prod'){
 	doit()
 
 }
+
+
+const path = require('path');
+
+function *walkSync(dir) {
+  const files = fsOrg.readdirSync(dir, { withFileTypes: true });
+  for (const file of files) {
+    if (file.isDirectory()) {
+      yield* walkSync(path.join(dir, file.name));
+    } else {
+      yield path.join(dir, file.name);
+    }
+  }
+}
+
+
+
+if (argv('action') == 'deploy_profile_editor'){
+
+	// get the config
+
+	//update the vue config with the deploy path
+	let doit = async function(){
+
+		let config = await fs.readFile('/app/util_config.json', 'utf8')
+		config = JSON.parse(config);
+
+		let usePrefix = config.profileEditorBasePath
+
+		for (const filePath of walkSync("/dist/profile-editor/assets/js/modules/")) {
+		  if (filePath.indexOf('.js')>-1){
+				const data = await fs.readFile(filePath, 'utf8');
+				if (data.indexOf('/api/') > -1 || data.indexOf('/ldp/') > -1){
+					let result = data.replace(/\/api\//g, `${usePrefix}api/`);
+					result = result.replace(/\/ldp\//g, `${usePrefix}ldp/`);
+					await fs.writeFile(filePath, result,'utf8');
+				}
+		  }
+		}
+	}
+	doit()
+}
+
+
+
+
--- util-service/deploy-profile-editor.sh
+++ util-service/deploy-profile-editor.sh
@@ -9,7 +9,9 @@ then
 	cd 'profile-edit'
 	cd 'source'
 	git pull
+	echo "npm installing"
 	npm install
+	echo "grunt running"
 	grunt
 else
 
@@ -18,7 +20,9 @@ else
 	git clone 'https://github.com/lcnetdev/profile-edit.git'
 	cd 'profile-edit'
 	cd 'source'
+	echo "npm installing"
 	npm install
+	echo "grunt running"
 	grunt    
 
 fi
@@ -27,18 +31,32 @@ FILE=/tmp/profile-editor-deploy/profile-edit/source/index.html
 if test -f "$FILE"; then
     echo "$FILE exists."
 
+    echo "Copying files"
+
 	rm -fr /dist/profile-editor/*
 	cp -R /tmp/profile-editor-deploy/profile-edit/source/* /dist/profile-editor/
+
+	echo "copied over"
 
     # remove this line from the index.html if it exists so it doesn't break our paths
     sed -e '/<base href="\/profile-edit\/">/d' /dist/profile-editor/index.html > /dist/profile-editor/index.tmp
     mv /dist/profile-editor/index.tmp /dist/profile-editor/index.html
+
+	echo "sed 1 finished"
+
 
     # inserting some JS so it labels when it is in the stage or prod region based on the url
     sed -e 's/<\/body>/<script>let x=document.createElement("span"); x.innerText=` -- ${window.location.pathname.includes("-stage") ? "STAGE" : "PROD"} REGION -- `; x.classList.add("normal"); x.style.color=`${window.location.pathname.includes("-stage") ? "#ffeb3b" : "lightgray"}`; document.getElementsByClassName("navbar-header")[0].appendChild(x);<\/script><\/body>/g' /dist/profile-editor/index.html > /dist/profile-editor/index.tmp
     cp /dist/profile-editor/index.tmp /dist/profile-editor/index.test
     mv /dist/profile-editor/index.tmp /dist/profile-editor/index.html
 
+	echo "sed 2 finished"
+
+
+    # fix the API paths to work with our settings
+    node /app/deploy-helper.js --action="deploy_profile_editor"
+
+	echo "deploy-helper finished"
 
 
 fi
