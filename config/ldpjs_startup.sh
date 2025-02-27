9f4a83b819b89198d3796e51cac10d914333a28c
Matthew Miller <thisismattmiller@gmail.com>
2025-02-27 10:46:22 -0500

    make sure ldpjs uses a static version of fast-xml module

--- config/ldpjs_startup.sh
+++ config/ldpjs_startup.sh
@@ -3,7 +3,9 @@
 # npm install -g nodemon
 echo "Starting up LDPJS"
 npm cache clean --force
-npm install --save cors fast-xml-parser strnum
+# npm install --save cors fast-xml-parser strnum
+npm install --save cors fast-xml-parser@3.18.0 strnum vary
+
 npm install 
 forever server.js
 # nodemon server.js --ignore '*.json' --ignore '*.xml' --ignore /app/tmp/ --ignore app/tmp/ --ignore tmp/
